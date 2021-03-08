762c4d118e9a0f1db55e1ab17efd9204f220ae1f
ronronscelestes <julie.plantey@gmail.com>
2021-03-08 08:45:22 +0100

    started gameCard partial - added css files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index');
+  let data = {
+    css : ['index', 'card']
+  }
+  res.render('index', data);
 });
 
 module.exports = router;
