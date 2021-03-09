4d7f741d5b476c96a9d2ba723e5cb8eb44d540b4
Romain B <romain.bnd@gmail.com>
2021-03-09 19:27:28 +0100

    some css improvements on one Game and creation of a function for the searchBars

--- public/javascripts/search-mainbar.js
+++ public/javascripts/search-mainbar.js
@@ -1,16 +1,30 @@
 const inputSearchMainBar = document.getElementById('main-search-bar');
 
+// export function searchBar(element, key, event) {
+//     element.addEventListener(event, function (evt) {
+//         if (evt.key === key) {
+//             const search = element.value;
+//             let query = `?name=${search}`;
+//             window.location = `/games/search${query}`;
+//             element.value = "";
+//         }
+//     })
+// };
 
-inputSearchMainBar.addEventListener('keypress', function (evt) {
-    if (evt.key === 'Enter') {
-        const search = inputSearchMainBar.value;
-        let query = `?name=${search}`;
+import {searchBar} from '/javascripts/search-navbar.js'
 
-        // axios.get(`/`)
-        //     .then(dataRes => {
-                window.location = `/games/search${query}`;
-                // console.log(dataRes);
-    //         })
-    //         .catch(dataErr => console.log(dataErr))
-    // }
-}})
+searchBar(inputSearchMainBar, 'Enter', 'keypress')
+
+// inputSearchMainBar.addEventListener('keypress', function (evt) {
+//     if (evt.key === 'Enter') {
+//         const search = inputSearchMainBar.value;
+//         let query = `?name=${search}`;
+
+//         // axios.get(`/`)
+//         //     .then(dataRes => {
+//                 window.location = `/games/search${query}`;
+//                 // console.log(dataRes);
+//     //         })
+//     //         .catch(dataErr => console.log(dataErr))
+//     // }
+// }})
